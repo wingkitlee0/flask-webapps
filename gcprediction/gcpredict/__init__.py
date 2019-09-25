6f03cb0113624abc0e4912d354f14dd4be3eb110
Kit Lee <wklee4993@gmail.com>
2019-09-25 17:20:08 -0400

    update

--- gcprediction/gcpredict/__init__.py
+++ gcprediction/gcpredict/__init__.py
@@ -1,3 +1,3 @@
 from flask import Flask
 app = Flask(__name__)
-from gcpredict import views
+from gcpredict import views, model, modelutil
--- gcprediction/gcpredict/views.py
+++ gcprediction/gcpredict/views.py
@@ -8,6 +8,8 @@ import os
 
 from gcpredict import app, model
 
+MODELFILE = 'data/clf.joblib'
+
 @app.route('/')
 @app.route("/test", methods=['GET', 'POST'])
 def main():
@@ -15,7 +17,7 @@ def main():
     timeString = now.strftime("%Y-%m-%d %H:%M")
     cpuCount = os.cpu_count()
 
-    mymodel = model.Model()
+    mymodel = model.Model(modelfile=os.path.join(app.static_folder, MODELFILE))
 
     templateData = {
         'title' : 'Green Card Predictor',
@@ -29,8 +31,22 @@ def main():
     elif request.method == 'POST':
         #nationality = request.args.getlist('myCountry')
         nationality = request.form['myCountry']
-        date = request.form['date1']
+        category = request.form['myCategory']
+        date1 = request.form['date1']
+        has_rfe = request.form['myRFE']
+
+        params = {
+            'nationality' : nationality,
+            'category' : category,
+            'date1' : date1,
+            'has_rfe' : has_rfe
+        }
+
+        prediction_date = mymodel.predict(**params)
         print(request.args)
-        resultText = "You are from {}. The date is {}".format(nationality, date)
-        results = {'text' : resultText}
+        resultText = "You are from {} and applied the GC on {}.".format(nationality, date1)
+        results = {
+            'text' : resultText,
+            'prediction_date' : prediction_date,
+        }
         return render_template('main.html', results=results, **templateData)
